d0d9b101f9dba0591d9ea63d1d0a0305280c5559
Michael Hawkins <BadgerCode@users.noreply.github.com>
2020-08-15 17:09:12 +0100

    Adds placeholder create addon command

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,18 @@ import { GModMenuProvider } from './GModMenuProvider';
 export function activate(context: vscode.ExtensionContext) {
 	console.log('Congratulations, your extension "gmod-sdk" is now active!');
 
+	// SIDE BAR
 	const gmodMenuProvider = new GModMenuProvider(vscode.workspace.rootPath);
 	vscode.window.registerTreeDataProvider('gmodMenuSideBar', gmodMenuProvider);
+
+
+	// COMMANDS
+
+	let createAddonCommand = vscode.commands.registerCommand('gmodsdk.createAddon', () => {
+		vscode.window.showInformationMessage('Creating GMod addon');
+	});
+
+	context.subscriptions.push(createAddonCommand);
 }
 
 
