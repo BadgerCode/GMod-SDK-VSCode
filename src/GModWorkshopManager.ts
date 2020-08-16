bc86ecbaa1412a1cc706a4e7da7155c5066a4175
Michael Hawkins <BadgerCode@users.noreply.github.com>
2020-08-16 16:38:31 +0100

    Adds ability to update existing workshop addon

--- src/GModWorkshopManager.ts
+++ src/GModWorkshopManager.ts
@@ -2,6 +2,8 @@ import * as vscode from 'vscode';
 import * as path from 'path';
 
 export class GModWorkshopManager {
+    private static DEBUGMODE: boolean = false;
+
     // TODO: Handle custom GMod folder locations & executable names
     private gmodFolderPath: string = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\garrysmod";
     private gmadExecutableName: string = "gmad.exe";
@@ -13,27 +15,69 @@ export class GModWorkshopManager {
         if (this.workspacePath == undefined)
             return;
 
-        vscode.window.showInputBox({ prompt: "This will create a NEW workshop addon. Continue? (yes/no)" })
-            .then(response => {
-                if (response == undefined || response.toLowerCase().trim() != "yes") {
-                    vscode.window.showInformationMessage(`You cancelled the workshop upload`);
-                    return;
-                }
+        var gmaPath = path.join(this.workspacePath!, "workshop-file.gma");
+        var thumbnailPath = path.join(this.samplesRoot, "workshop-thumbnail.jpg");
 
-                var gmaPath = path.join(this.workspacePath!, "workshop-file.gma");
-                var thumbnailPath = path.join(this.samplesRoot, "workshop-thumbnail.jpg");
+        if (GModWorkshopManager.DEBUGMODE) {
+            console.log("Pretending to upload GMA");
+            console.log(`GMA: ${gmaPath}`);
+            console.log(`Thumbnail: ${thumbnailPath}`);
+            return;
+        }
 
-                // TODO: Re-use terminal if exists/cleanup terminal on exit
-                var terminal = vscode.window.createTerminal("GMod Publish");
-                terminal.show();
+        // TODO: Re-use terminal if exists/cleanup terminal on exit
+        var terminal = vscode.window.createTerminal("GMod Publish");
+        terminal.show();
 
-                var gmadPath = path.join(this.gmodFolderPath, "bin", this.gmadExecutableName);
-                terminal.sendText(`& "${gmadPath}" create -folder "${this.workspacePath}" -out "${gmaPath}" -warninvalid`);
+        var gmadPath = path.join(this.gmodFolderPath, "bin", this.gmadExecutableName);
+        terminal.sendText(`& "${gmadPath}" create -folder "${this.workspacePath}" -out "${gmaPath}" -warninvalid`);
 
-                var gmpublishPath = path.join(this.gmodFolderPath, "bin", this.gmpublishExecutableName);
-                terminal.sendText(`& "${gmpublishPath}" create -addon "${gmaPath}" -icon "${thumbnailPath}"`);
+        var gmpublishPath = path.join(this.gmodFolderPath, "bin", this.gmpublishExecutableName);
+        terminal.sendText(`& "${gmpublishPath}" create -addon "${gmaPath}" -icon "${thumbnailPath}"`);
 
-                // TODO: Wait for terminal to finish processing commands and delete the .gma file
-            });
+        // TODO: Wait for terminal to finish processing commands and delete the .gma file
+    }
+
+    updateAddon(fileID: string): void {
+        if (this.workspacePath == undefined)
+            return;
+
+        var gmaPath = path.join(this.workspacePath!, "workshop-file.gma");
+        var thumbnailPath = path.join(this.samplesRoot, "workshop-thumbnail.jpg");
+
+        if (GModWorkshopManager.DEBUGMODE) {
+            console.log(`Pretending to upload GMA to existing addon ${fileID}`);
+            console.log(`GMA: ${gmaPath}`);
+            console.log(`Thumbnail: ${thumbnailPath}`);
+            return;
+        }
+
+        var terminal = vscode.window.createTerminal("GMod Publish");
+        terminal.show();
+
+        var gmadPath = path.join(this.gmodFolderPath, "bin", this.gmadExecutableName);
+        terminal.sendText(`& "${gmadPath}" create -folder "${this.workspacePath}" -out "${gmaPath}" -warninvalid`);
+
+        var gmpublishPath = path.join(this.gmodFolderPath, "bin", this.gmpublishExecutableName);
+        terminal.sendText(`& "${gmpublishPath}" update -id ${fileID} -addon "${gmaPath}"`);
+
+        // TODO: Wait for terminal to finish processing commands and delete the .gma file
+    }
+
+    updateAddonThumbnail(fileID: string, thumbnailPath: string): void {
+        if (GModWorkshopManager.DEBUGMODE) {
+            console.log(`Pretending to update existing addon ${fileID} thumbnail`);
+            console.log(`Thumbnail: ${thumbnailPath}`);
+            return;
+        }
+
+
+        var terminal = vscode.window.createTerminal("GMod Publish");
+        terminal.show();
+
+        var gmpublishPath = path.join(this.gmodFolderPath, "bin", this.gmpublishExecutableName);
+        terminal.sendText(`& "${gmpublishPath}" update -id ${fileID} -icon "${thumbnailPath}"`);
+
+        // TODO: Wait for terminal to finish processing commands and delete the .gma file
     }
 }
--- src/extension.ts
+++ src/extension.ts
@@ -52,7 +52,49 @@ export function activate(context: vscode.ExtensionContext) {
 			return;
 		}
 
-		workshopManager.upload();
+		vscode.window
+			.showQuickPick(["NEW", "Existing Addon"], { placeHolder: "Upload a new addon or update an existing addon?" })
+			.then(response => {
+				if (response == undefined)
+					return;
+
+				var shouldUploadNewAddon = response == "NEW";
+				if (shouldUploadNewAddon) {
+					vscode.window.showInputBox({ prompt: "This will upload a NEW addon to the workshop. Type YES to continue", placeHolder: "YES" })
+						.then(confirmation => {
+							if (confirmation == undefined || confirmation.toLowerCase().trim() != "yes") {
+								vscode.window.showInformationMessage(`You cancelled the workshop upload`);
+								return;
+							}
+							workshopManager.upload();
+						});
+				}
+				else {
+					vscode.window.showInputBox({ prompt: "Enter the workshop file ID. This can be found at the end of the URL. E.g. https://steamcommunity.com/sharedfiles/filedetails/?id=2086515808 has a file ID of 2086515808" })
+						.then(fileID => {
+							if (fileID == undefined) {
+								vscode.window.showInformationMessage(`You cancelled the workshop upload`);
+								return;
+							}
+
+							var trimmedFileID = fileID.trim();
+							if (isNaN(Number(trimmedFileID))) {
+								vscode.window.showInformationMessage(`"${trimmedFileID}" is not a valid workshop File ID. E.g. https://steamcommunity.com/sharedfiles/filedetails/?id=2086515808 has a file ID of 2086515808`);
+								return;
+							}
+
+							vscode.window.showInputBox({ prompt: `This will UPDATE the addon ${trimmedFileID}. Type YES to continue`, placeHolder: "YES" })
+								.then(confirmation => {
+									if (confirmation == undefined || confirmation.toLowerCase().trim() != "yes") {
+										vscode.window.showInformationMessage(`You cancelled the workshop upload`);
+										return;
+									}
+
+									workshopManager.updateAddon(trimmedFileID);
+								});
+						});
+				}
+			});
 	});
 	context.subscriptions.push(uploadWorkshopCommand);
 
